51c965ca953dee376e9aa777f665f61487f08da1
Sarunas Zube <sarunas.zube@gmail.com>
2025-12-31 09:21:09 +0000

    chore: Update Vite configuration to enhance server performance and fix hmr issues

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+    },
   },
 })
